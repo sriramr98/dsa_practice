b73e1b9cb2ab2a47f93156359f00d599ac58f19d
R.Sriram <r.sriram@games24x7.com>
2023-08-05 21:40:24 +0530

    added DoublyLinkedList and LFUCache

---
+++ src/main/java/org/example/algos/LFUCache.java
@@ -0,0 +1,118 @@
+package org.example.algos;
+
+import org.example.dsa.linkedlist.DoublyLinkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LFUCache {
+
+    private final int capacity;
+    private final HashMap<Integer, DoublyLinkedList<Integer, LFUNode>> freqMap;
+    private final HashMap<Integer, DoublyLinkedList.LinkedListNode<Integer, LFUNode>> nodeCache;
+    int minFrequency = 0;
+
+    public static class LFUNode {
+        private int value;
+        private int freq;
+
+        public LFUNode(int value) {
+            this.value = value;
+            this.freq = 1;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getFreq() {
+            return freq;
+        }
+
+        public void setFreq(int freq) {
+            this.freq = freq;
+        }
+    }
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        freqMap = new HashMap<>();
+        nodeCache = new HashMap<>();
+    }
+
+    public void set(int key, int value) {
+        if (this.get(key) == -1) {
+            // new entry
+            evictIfRequired();
+
+            // Since new key and not used yet, default frequency to 1
+            minFrequency = 1;
+            DoublyLinkedList<Integer, LFUNode> currentMinFrequencyList = freqMap.getOrDefault(minFrequency, new DoublyLinkedList<>());
+            DoublyLinkedList.LinkedListNode<Integer, LFUNode> insertedNode = currentMinFrequencyList.insertAtTail(key, new LFUNode(value));
+            nodeCache.put(key, insertedNode);
+            freqMap.put(minFrequency, currentMinFrequencyList);
+        } else {
+            // old entry
+            // Doubt: Do we increase frequency if updating value of existing node?
+            // Implementation: Assuming we only update frequency for GET
+
+            DoublyLinkedList.LinkedListNode<Integer, LFUNode> node = nodeCache.get(key);
+            node.getValue().setValue(value);
+        }
+    }
+
+    public int get(int key) {
+        if (!nodeCache.containsKey(key)) {
+            return -1;
+        }
+
+        DoublyLinkedList.LinkedListNode<Integer, LFUNode> node = nodeCache.get(key);
+        int currentNodeFreq = node.getValue().getFreq();
+        DoublyLinkedList<Integer, LFUNode> currentFreqList = freqMap.get(currentNodeFreq);
+
+        currentFreqList.removeNode(node);
+
+        int nextFrequency = currentNodeFreq + 1;
+        node.getValue().setFreq(nextFrequency);
+
+        DoublyLinkedList<Integer, LFUNode> nextFreqList = freqMap.getOrDefault(nextFrequency, new DoublyLinkedList<>());
+        DoublyLinkedList.LinkedListNode<Integer, LFUNode> insertedNode = nextFreqList.insertAtTail(key, node.getValue());
+
+        nodeCache.put(key, insertedNode);
+        freqMap.put(nextFrequency, nextFreqList);
+
+        return node.getValue().getValue();
+    }
+
+    private void evictIfRequired() {
+        if (capacity > nodeCache.size() || nodeCache.isEmpty()) {
+            return;
+        }
+
+        if (minFrequency == 0 || !freqMap.containsKey(minFrequency)) {
+            return;
+        }
+
+        DoublyLinkedList<Integer, LFUNode> minFreqList = freqMap.get(minFrequency);
+        if (minFreqList.size == 0) {
+            minFrequency++;
+            evictIfRequired();
+            return;
+        }
+
+        DoublyLinkedList.LinkedListNode<Integer, LFUNode> deletedNode = minFreqList.removeHead();
+        nodeCache.remove(deletedNode.getKey());
+    }
+
+    void print() {
+        for (Map.Entry<Integer, DoublyLinkedList.LinkedListNode<Integer, LFUNode>> entry : nodeCache.entrySet()) {
+            System.out.print("(" + entry.getKey() + ", " + entry.getValue().getValue() + ")");
+        }
+        System.out.println("");
+    }
+
+}
---
+++ src/main/java/org/example/dsa/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,143 @@
+package org.example.dsa.linkedlist;
+
+public class DoublyLinkedList<KEY, VALUE> {
+
+    public static class LinkedListNode<K, V> {
+        K key;
+        V value;
+        LinkedListNode<K, V> next;
+        LinkedListNode<K, V> prev;
+
+        public LinkedListNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public LinkedListNode<K, V> getNext() {
+            return next;
+        }
+
+        public LinkedListNode<K, V> getPrev() {
+            return prev;
+        }
+
+        public void setNext(LinkedListNode<K, V> next) {
+            this.next = next;
+        }
+
+        public void setPrev(LinkedListNode<K, V> prev) {
+            this.prev = prev;
+        }
+    }
+
+    public LinkedListNode<KEY, VALUE> head;
+    public LinkedListNode<KEY, VALUE> tail;
+    public int size;
+
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public LinkedListNode<KEY, VALUE> insertAtHead(KEY key, VALUE data) {
+        LinkedListNode<KEY, VALUE> newNode = new LinkedListNode<>(key, data);
+        if(this.head == null){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        this.size++;
+
+        return newNode;
+    }
+
+    public LinkedListNode<KEY, VALUE> insertAtTail(KEY key, VALUE data) {
+        LinkedListNode<KEY, VALUE> newNode = new LinkedListNode<>(key, data);
+        if (this.tail == null){
+            this.tail = newNode;
+            this.head = newNode;
+            newNode.next = null;
+            newNode.prev = null;
+        }
+
+        else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+            newNode.next = null;
+        }
+        this.size++;
+
+        return newNode;
+    }
+
+    public LinkedListNode<KEY, VALUE> getHead(){
+        return this.head;
+    }
+
+    public LinkedListNode<KEY, VALUE> getTail(){
+        return this.tail;
+    }
+
+    public LinkedListNode<KEY, VALUE> removeNode(LinkedListNode<KEY, VALUE> node){
+        if(node == null){
+            return null;
+        }
+
+        if(node.prev != null){
+            node.prev.next = node.next;
+        }
+
+        if(node.next != null){
+            node.next.prev = node.prev;
+        }
+
+        if(node == this.head){
+            this.head = this.head.next;
+        }
+        if(node == this.tail){
+            this.tail = this.tail.prev;
+        }
+        this.size--;
+        return node;
+    }
+
+    public void remove(VALUE data){
+        LinkedListNode<KEY, VALUE> i = this.getHead();
+        while(i != null){
+            if(i.value.equals(data)){
+                this.removeNode(i);
+            }
+            i = i.next;
+        }
+    }
+
+    public LinkedListNode<KEY, VALUE> removeHead(){
+        return this.removeNode(this.head);
+    }
+
+    public LinkedListNode<KEY, VALUE> removeTail(){
+        return this.removeNode(this.tail);
+    }
+}
